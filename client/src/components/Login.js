6f44e9dd589ccc319a2d09157c89639ec16cd577
cladams0203 <cladams0203@gmail.com>
2019-12-13 21:31:38 -0500

    finished protected route and changed password to 1234

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
     const [form, setForm] = useState({
       username:'',
       password: ''
     })
+
+    const handleSubmit = () =>{
+      axiosWithAuth().post('/api/login', form)
+        .then(res => {
+          localStorage.setItem('token', res.data.payload)
+          setForm({username:'', password: ''})
+          props.history.push('/bubbles')
+        })
+        .catch(err => console.log(err))
+    }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <form onSubmit={(e) => {
         e.preventDefault()
-        
+        handleSubmit()
+
       }}>
         <input 
           name='username'
