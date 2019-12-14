6ecc720e75e6d805df392715b230d270115cd6a0
cladams0203 <cladams0203@gmail.com>
2019-12-13 21:10:07 -0500

    finished login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+    const [form, setForm] = useState({
+      username:'',
+      password: ''
+    })
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        
+      }}>
+        <input 
+          name='username'
+          placeholder='User Name'
+          value={form.username}
+          onChange={(e) => {
+            setForm({...form, [e.target.name]: e.target.value})
+          }}
+        />
+        <input 
+          name='password'
+          type='password'
+          placeholder='Password'
+          value={form.password}
+          onChange={(e) => {
+            setForm({...form, [e.target.name]: e.target.value})
+          }}
+        />
+        <button type='submit'>Login</button>
+      </form>
     </>
   );
 };
